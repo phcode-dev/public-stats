b37aa3fab4dbd77d2d5b4cac00509dbf470b6931
abose <arun@core.ai>
2024-08-21 11:57:18 +0530

    download counts scripts

---
+++ build/downloadCounts.js
@@ -0,0 +1,69 @@
+const fs = require('fs');
+const {RELEASE_ASSET_INSTALLER_SUFFIXES, RELEASE_ASSET_UPDATER_SUFFIXES} = require("./constants");
+
+const ALL_INSTALLER_SUFFIXES = Object.values(RELEASE_ASSET_INSTALLER_SUFFIXES);
+const ALL_UPDATER_SUFFIXES = Object.values(RELEASE_ASSET_UPDATER_SUFFIXES);
+function hasSuffix(assetName, suffixList) {
+    // only installer and updater download names are allowed.
+    for(let suffix of suffixList) {
+        if(assetName.endsWith(suffix)){
+            return true;
+        }
+    }
+    return false;
+}
+
+function computeTotalDownloads(releases) {
+    let totalInstallerDownloads = 0;
+    let totalUpdaterDownloads = 0;
+    releases.forEach(release => {
+        if (!release.prerelease) {
+            release.assets.forEach(asset => {
+                if(hasSuffix(asset.name, ALL_INSTALLER_SUFFIXES)) {
+                    totalInstallerDownloads += asset.download_count;
+                }
+                if(hasSuffix(asset.name, ALL_UPDATER_SUFFIXES)) {
+                    totalUpdaterDownloads += asset.download_count;
+                }
+            });
+        }
+    });
+    return {totalInstallerDownloads, totalUpdaterDownloads};
+}
+
+function getCurrentDownloadData() {
+    try{
+        const jsonText = fs.readFileSync('docs/download_counts.json');
+        const dataObj = JSON.parse(jsonText);
+        return dataObj;
+    } catch (e) {
+        console.error(e);
+    }
+    return null;
+}
+
+function updateDownloadStats(releases) {
+    const {totalInstallerDownloads, totalUpdaterDownloads} = computeTotalDownloads(releases);
+
+    const data = {
+        timestamp: new Date(),
+        totalDownloads: totalInstallerDownloads + totalUpdaterDownloads,
+        totalDownloadRatePerMinute: 0,
+        installerDownloads: totalInstallerDownloads,
+        updateDownloads: totalUpdaterDownloads,
+        "downloadProofLink": "https://github.com/phcode-dev/public-stats"
+    };
+    const existingData = getCurrentDownloadData();
+    if (existingData && existingData.timestamp && existingData.totalDownloads) {
+        const lastTimeStamp = new Date(existingData.timestamp).getTime();
+        const currentTimeStamp = new Date(data.timestamp).getTime();
+        const timeDifferenceMinutes = (currentTimeStamp - lastTimeStamp) / (1000 * 60);
+        const downloadDifference = data.totalDownloads - existingData.totalDownloads;
+        data.totalDownloadRatePerMinute = timeDifferenceMinutes ?
+            Math.round(downloadDifference / timeDifferenceMinutes): 0;
+    }
+
+    fs.writeFileSync('docs/download_counts.json', JSON.stringify(data, null, 2));
+}
+
+exports.updateDownloadStats = updateDownloadStats;
---
+++ build/index.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const downloadCounts = require("./downloadCounts");
+
+async function getReleaseDetails() {
+    const headers = {
+        'Accept': 'application/vnd.github.v3+json'
+    };
+    if (process.env.GITHUB_TOKEN) {
+        console.log("Using GITHUB_TOKEN from process.env.GITHUB_TOKEN");
+        headers['Authorization'] = `token ${process.env.GITHUB_TOKEN}`;
+    } else {
+        console.warn('process.env.GITHUB_TOKEN not found. Calling GitHub API without auth!');
+    }
+
+    let releases = [];
+    let nextPage = 'https://api.github.com/repos/phcode-dev/phoenix-desktop/releases?per_page=100';  // Start with the first page
+
+    while (nextPage) {
+        const response = await fetch(nextPage, { headers });
+        const data = await response.json();
+        releases = releases.concat(data);
+
+        const linkHeader = response.headers.get('link');
+        nextPage = null; // Reset nextPage
+        if (linkHeader) {
+            const links = linkHeader.split(',').map(a => a.split(';'));
+            const nextLink = links.find(link => link[1].includes('rel="next"'));
+            if (nextLink) {
+                nextPage = nextLink[0].trim().slice(1, -1); // Slice to remove the angle brackets
+            }
+        }
+    }
+    return releases;
+}
+
+
+async function updateDocs() {
+    const releases = await getReleaseDetails();
+    // for debugging uncomment the below lines  and comment the github fetch line
+    // fs.writeFileSync('temp/release.json', JSON.stringify(releases, null, 2)); // only do this once and comment
+    //const releases = JSON.parse(fs.readFileSync('temp/release.json'));
+    downloadCounts.updateDownloadStats(releases);
+}
+
+updateDocs();
